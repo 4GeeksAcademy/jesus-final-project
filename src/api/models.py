bfeb826aea440b6375d21e20b9cabf2fb126eb5b
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-06-24 02:14:15 +0200

    Usuario x User —> error sintaxis

--- src/api/models.py
+++ src/api/models.py
@@ -26,13 +26,13 @@ class Articulos(db.Model):  # corregido db.model a db.Model
 
     id: Mapped[int] = mapped_column(primary_key=True)
     nombre_articulo: Mapped[str] = mapped_column(String(25), unique=True, nullable=False)
-    caracteristicas: Mapped[str] = mapped_column(String(200), unique=True)
+    caracteristicas: Mapped[str] = mapped_column(String(200))
     cantidad: Mapped[int] = mapped_column(nullable=True, default=1)
     estado: Mapped[str] = mapped_column(String(50), nullable=False)
     categoria: Mapped[str] = mapped_column(String(50), nullable=False)
 
     usuario_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
-    usuario: Mapped['Usuario'] = relationship('Usuario', back_populates='articulos')
+    usuario: Mapped['User'] = relationship('User', back_populates='articulos')
 
     articulo_favorito_id: Mapped[int] = mapped_column(ForeignKey('articulo_favorito.id'))
     articulo_favorito: Mapped['Articulo_Favorito'] = relationship('Articulo_Favorito', back_populates='articulos')
