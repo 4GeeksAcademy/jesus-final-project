49878adb5345c5f3f9b9b3fd4e4eba318e105839
Elena <pitufa247@gmail.com>
2025-06-25 07:43:27 +0000

    art fav

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey, Integer
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -9,6 +9,18 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+class ArticuloFavorito(db.Model):
+    __tablename__ = 'articulo_favorito'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    usuario_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
+    articulo_id: Mapped[int] = mapped_column(ForeignKey('articulo.id'))
+
+   # usuario: Mapped['Usuario'] = relationship(back_populates='articulo_favorito')
+   # articulo: Mapped['Articulo'] = relationship(back_populates='a')
+
+    def articulos_favoritos(self):
+        return [rel.articulo for rel in self.articulos_favoritos_rel]
 
 
     def serialize(self):
