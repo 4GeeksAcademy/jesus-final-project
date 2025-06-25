38912896282bc6602deec1c0bd0bfdc10f589e61
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-06-25 18:54:17 +0000

    modelview

--- src/api/admin.py
+++ src/api/admin.py
@@ -10,45 +10,51 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    admin.add_view(UserView(User, db.session))
+    admin.add_view(UserModelView(User, db.session))
+    admin.add_view(ArticulosModelView(Articulos, db.session))
+    admin.add_view(Articulo_favoritoModelView(Articulo_favorito, db.session))
+    admin.add_view(TransaccionTruekeModelView(TransaccionTrueke, db.session))
+    admin.add_view(ComentariosModelView(Comentarios, db.session))
+    admin.add_view(DatosPersonalesModelView(DatosPersonales, db.session))
+    admin.add_view(RatingModelView(Rating, db.session))
 
 
-class UserView(ModelView):
+class UserModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'nombre_de_usuario', 'email', 'password_hash', 'is_active', 'articulos', 'articulos_favoritos',
                    'ratings', 'datos_personales', 'comentarios', 'transacciones_como_propietario', 'transacciones_como_receptor']
 
 
-class Articulos(ModelView):
+class ArticulosModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'titulo', 'caracteristicas', 'estado', 'modelo', 'cantidad', 'categoria',
                    'img', 'usuario_id', 'usuario', 'articulos_favoritos', 'ratings']
 
 
-class Articulo_favorito(ModelView):
+class Articulo_favoritoModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'usuario_id', 'usuario', 'articulo_id', 'articulo']
 
 
-class TransaccionTrueke(ModelView):
+class TransaccionTruekeModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'comentarios', 'articulo_id_propietario', 'articulo_id_receptor', 'usuario_propietario_id', 'usuario_receptor_id',
                    'articulo_propietario', 'articulo_receptor', 'propietario', 'receptor', 'comentarios_transaccion']
 
 
-class Comentarios(ModelView):
+class ComentariosModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'comentario', 'comentario_user_id',
                    'transaccion_id', 'usuario', 'transaccion']
 
 
-class DatosPersonales(ModelView):
+class DatosPersonalesModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'nombre_completo', 'telefono',
                    'direccion', 'fecha_registro', 'img', 'user_id', 'usuario']
 
 
-class Rating(ModelView):
+class RatingModelView(ModelView):
     column_auto_selected = True
     column_list = ['id', 'valoraciones', 'comentarios',
                    'usuario_id', 'usuario', 'articulo_id', 'articulo']
