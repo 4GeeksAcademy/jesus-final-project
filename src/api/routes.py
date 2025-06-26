28c63a5806480a81c5379b37dbc119517a91cacc
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-06-26 09:55:41 +0000

    Edit Datos Personales Query

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, Usuario
+from api.models import db, Usuario, DatosPersonales
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,26 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/editar/datos-personales', methods=['PUT'])
+def editar_datos_personales():
+    body = request.get_json(silent=True)
+    if not body:
+        return jsonify({'msg': 'necesitas completar los datos'}), 400
+    if 'nombre_completo' not in body:
+        return jsonify({'msg': 'necesitas completar enviar tu nombre para poder editar tu perfil'}), 400
+    if 'telefono' not in body:
+        return jsonify({'msg': 'necesitas completar enviar tu teléfono para poder editar tu perfil'}), 400
+    if 'direccion' not in body:
+        return jsonify({'msg': 'necesitas completar enviar tu dirección para poder editar tu perfil'}), 400
+
+    datos_personales = DatosPersonales()
+    datos_personales.nombre_completo = body['nombre_completo']
+    datos_personales.telefono = body['telefono']
+    datos_personales.direccion = body['direccion']
+    datos_personales.img = body['img']
+
+    db.session.add(datos_personales)
+    db.session.commit()
+    return jsonify({'msg': 'datos personales editados correctamente'}), 200
