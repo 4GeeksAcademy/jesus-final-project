4ed35793716891c099152cc3ba3063b8348a6cd8
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-06-27 18:44:10 +0000

    EMAIL + register. V1

--- src/app.py
+++ src/app.py
@@ -6,14 +6,17 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, Usuario
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_mail import Mail
+from flask_mail import Message
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 # from models import Person
 
@@ -22,9 +25,19 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config["JWT_SECRET_KEY"] = "super-secret"  
+app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
-
+app.config.update(dict(
+    DEBUG=False,
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USE_SSL=False,
+    MAIL_USERNAME='trueketeo@gmail.com',
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+))
+mail = Mail(app)
+bcrypt = Bcrypt(app)
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -63,6 +76,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -70,6 +85,41 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    body = request.get_json(silent=True)
+
+    if not body:
+        return jsonify({'msg': 'you have to send something'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'the email the password filed is obligatory'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'the password filed is obligatory'}), 400
+
+    new_user = Usuario()
+    new_user.nombre_de_usuario = body['nombre_de_usuario']
+    new_user.email = body['email']
+    new_user.password = bcrypt.generate_password_hash(
+        body['password']).decode("utf-8")
+    new_user.is_active = True
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'msg': f'user {new_user.email} created correctly'}), 201
+
+
+@app.route('/enviar-mensaje', methods=['GET'])
+def enviar_mensaje():
+    msg = Message(
+        subject="Hello",
+        sender="Trueketeo@gmail.com",
+        recipients=["Trueketeo@gmail.com"],
+    )
+    msg.html = "<b>HOLA!</b>"
+    mail.send(msg)
+    return jsonify({'msg': 'Mail enviado correctamente'}), 201
 
 
 # this only runs if `$ python src/main.py` is executed
