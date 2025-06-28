802fc970d77b36145178104cd383030fe545bcf2
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-06-28 11:03:51 +0000

    Branch creada

---
+++ src/front/pages/DatosPersonales.jsx
@@ -0,0 +1,13 @@
+// Import necessary components from react-router-dom and other parts of the application.
+import { Link } from "react-router-dom";
+
+export const DatosPersonales = () => {
+
+
+
+  return (
+  <div>
+
+  </div>
+  );
+};
