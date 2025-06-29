5cd82ea60095e1524a6179a3865179e65fca3a3a
Elena <pitufa247@gmail.com>
2025-06-29 22:50:09 +0000

    endpoint publicar

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -11,6 +11,7 @@ import { Single } from "./pages/Single";
 import { DatosPersonales } from "./pages/DatosPersonales";
 import { Articulo } from "./pages/Articulo";
 import { LoginRegistro } from "./pages/LoginRegistro";
+import { PublicarArticulo } from "./pages/PublicarArticulo";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,6 +30,7 @@ export const router = createBrowserRouter(
       <Route path="/datospersonales/:usuarioId" element={<DatosPersonales />} />
       <Route path="/articulo/:articuloId" element={<Articulo />} />
       <Route path="/identificate" element={<LoginRegistro />} />
+      <Route path="/publicar" element={<PublicarArticulo />} />
     </Route>
   )
 );
