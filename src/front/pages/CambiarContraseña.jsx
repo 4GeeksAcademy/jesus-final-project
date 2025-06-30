e100ec7ebf639cca41e7388db31831856956d4f2
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-06-30 10:49:34 +0000

    EMAIL HTML y Cambiar Contraseña HTML

---
+++ src/front/pages/CambiarContraseña.jsx
@@ -0,0 +1,9 @@
+// Import necessary components from react-router-dom and other parts of the application.
+import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
+
+export const CambiarContraseña = () => {
+  // Access the global state and dispatch function using the useGlobalReducer hook.
+
+ 
+};
